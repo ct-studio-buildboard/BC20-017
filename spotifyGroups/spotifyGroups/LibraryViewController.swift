3c1f5958e0be7cb9e560c5dc5317505094648fdd
Jae Min Cha <jaemincha@Jaes-MacBook-Pro.local>
2020-04-21 22:30:39 +0900

    linking backend and frontend

--- spotifyGroups/spotifyGroups/LibraryViewController.swift
+++ spotifyGroups/spotifyGroups/LibraryViewController.swift
@@ -10,12 +10,46 @@ import UIKit
 
 class LibraryViewController: UIViewController {
 
-
     @IBOutlet weak var groups_scroll: UIScrollView!
     override func viewDidLoad() {
         super.viewDidLoad()
         groups_scroll.showsVerticalScrollIndicator = false
         // Do any additional setup after loading the view.
+        let name = userName.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed)!
+        
+        // Prepare URL
+        let url = URL(string: "http://spotify-env.eba-iqymqugf.us-west-2.elasticbeanstalk.com/api/getGroups/" + name)
+        guard let requestUrl = url else { fatalError() }
+        
+        // Prepare URL Request Object
+        let request = URLRequest(url: requestUrl)
+        
+        // Perform HTTP Request
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+                
+            // Check for Error
+            if let error = error {
+                print("Error took place \(error)")
+                return
+            }
+     
+            // Convert HTTP Response Data to a String / JSON
+            if let data = data, let dataString = String(data: data, encoding: .utf8) {
+                print("Response data string:\n \(dataString)")
+                do {
+                    if let jsonArray = try JSONSerialization.jsonObject(with: data, options : .allowFragments) as? [Dictionary<String,Any>]
+                    {
+                       print(jsonArray) // use the json here
+                    } else {
+                        print("bad json")
+                    }
+                } catch let error as NSError {
+                    print(error)
+                }
+            }
+        }
+        
+        task.resume()
     }
     
 
