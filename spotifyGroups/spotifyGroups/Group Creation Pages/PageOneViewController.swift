471ec1d78d5259a7315a63e45ae0f103c108281b
Jae Min Cha <jaemincha@Jaes-MacBook-Pro.local>
2020-04-23 20:35:53 +0900

    placeholder text for group creation

--- spotifyGroups/spotifyGroups/Group Creation Pages/PageOneViewController.swift
+++ spotifyGroups/spotifyGroups/Group Creation Pages/PageOneViewController.swift
@@ -8,54 +8,31 @@
 
 import UIKit
 
-class PageOneViewController: UIViewController {
+class PageOneViewController: UIViewController, UITextViewDelegate {
 
     @IBOutlet weak var groupName: UITextView!
+    @IBOutlet weak var groupDesc: UITextView!
     
     @IBOutlet weak var main_view: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         main_view.layer.cornerRadius = 8;
         main_view.layer.masksToBounds = true;
-        // Do any additional setup after loading the view.
-        let name = userName.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed)!
         
-        // Prepare URL
-        let url = URL(string: "http://spotify-env.eba-iqymqugf.us-west-2.elasticbeanstalk.com/api/initCreation/" + name)
-        guard let requestUrl = url else { fatalError() }
+        // For placeholder text in Group Name and Group Description
+        groupName.delegate = self
+        groupName.text = "Group Name"
+        groupName.textColor = UIColor.lightGray
         
-        // Prepare URL Request Object
-        let request = URLRequest(url: requestUrl)
-        
-        // Perform HTTP Request
-        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
-                
-                // Check for Error
-                if let error = error {
-                    print("Error took place \(error)")
-                    return
-                }
-         
-                // Convert HTTP Response Data to a String / JSON
-                if let data = data, let dataString = String(data: data, encoding: .utf8) {
-                    print("Response data string:\n \(dataString)")
-                    
-                    do {
-                        if let jsonArray = try JSONSerialization.jsonObject(with: data, options : .allowFragments) as? [Dictionary<String,Any>]
-                        {
-                           print(jsonArray) // use the json here
-                        } else {
-                            print("bad json")
-                        }
-                    } catch let error as NSError {
-                        print(error)
-                    }
-                }
-        }
-        
-        task.resume()
+        groupDesc.delegate = self
+        groupDesc.text = "Group Description"
+        groupDesc.textColor = UIColor.lightGray
     }
     
-
-
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if textView.textColor == UIColor.lightGray {
+            textView.text = ""
+            textView.textColor = UIColor.white
+        }
+    }
 }
