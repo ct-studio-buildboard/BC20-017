be04590e22749bc6b8e83a776ee77ffadf2b5768
Jae Min Cha <jaemincha@Jaes-MacBook-Pro.local>
2020-04-23 11:53:39 +0900

    slight modification

--- spotifyGroups/spotifyGroups/Group Creation Pages/PageTwoViewController.swift
+++ spotifyGroups/spotifyGroups/Group Creation Pages/PageTwoViewController.swift
@@ -71,8 +71,39 @@ class PageTwoViewController: UIViewController {
     
     @objc func buttonAction(sender: UIButton!) {
         self.dismiss(animated: true, completion: {
-            self.presentingViewController?.dismiss(animated: true, completion: nil)
-      })
+            self.presentingViewController?.dismiss(animated: true, completion: nil)})
+        
+        // Prepare URL
+        let url = URL(string: "http://spotify-env.eba-iqymqugf.us-west-2.elasticbeanstalk.com/api/createGroup/")
+        guard let requestUrl = url else { fatalError() }
+        
+        // Prepare URL Request Object
+        var request = URLRequest(url: requestUrl)
+        request.httpMethod = "POST"
+         
+        // HTTP Request Parameters which will be sent in HTTP Request Body
+        // let postString = "userName=" + userName + "&groupName=" + + "&groupDesc=" +
+        let postString = "userName=test"
+        
+        // Set HTTP Request Body
+        request.httpBody = postString.data(using: String.Encoding.utf8);
+        
+        // Perform HTTP Request
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+                
+                // Check for Error
+                if let error = error {
+                    print("Error took place \(error)")
+                    return
+                }
+         
+                // Convert HTTP Response Data to a String
+                if let data = data, let dataString = String(data: data, encoding: .utf8) {
+                    print("Response data string:\n \(dataString)")
+                }
+        }
+        
+        task.resume()
     }
     
     @objc func oneTapped(tapGestureRecognizer: UITapGestureRecognizer)
