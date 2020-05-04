afe3b08b67183f6009619e015252ca414dcd5e79
Jae Min Cha <jaemincha@Jaes-MacBook-Pro.local>
2020-05-04 22:30:26 +0900

    updated version for user 1

---
+++ spotifyGroups/spotifyGroups/Group Creation Pages/PageTwoSubViewController.swift
@@ -0,0 +1,34 @@
+//
+//  PageTwoSubViewController.swift
+//  spotifyGroups
+//
+//  Created by Jae Min Cha on 04/05/2020.
+//  Copyright © 2020 Spotify. All rights reserved.
+//
+
+import UIKit
+
+class PageTwoSubViewController: UIViewController {
+
+    @IBOutlet weak var wechat: UIView!
+    @IBOutlet weak var facebook: UIView!
+    @IBOutlet weak var slack: UIView!
+    @IBOutlet weak var instagram: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
